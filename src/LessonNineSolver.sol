a1b64260ae40f2bd0cc8aa4a97103c1449215c5c
Lulox <ligma@duck.com>
2023-06-28 06:09:55 -0300

    Fixed error on correctAnswer that prevented minting

--- src/LessonNineSolver.sol
+++ src/LessonNineSolver.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
@@ -10,7 +10,7 @@ import {LessonNine} from "./LessonNine.sol";
 contract LessonNineSolver is IERC721Receiver, Ownable {
     function solveLessonNine(address targetContract, string memory twitterHandle) public onlyOwner {
         uint256 correctAnswer =
-            uint256(keccak256(abi.encodePacked(msg.sender, block.prevrandao, block.timestamp))) % 100000;
+            uint256(keccak256(abi.encodePacked(address(this), block.prevrandao, block.timestamp))) % 100000;
 
         LessonNine(targetContract).solveChallenge(correctAnswer, twitterHandle);
     }
